d7c8b8eb1ee3036acd8306a84fdacba7b85d3697
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-03-03 19:01:10 -0300

    indent code

--- src/main/kotlin/br/com/rodrigo/forum/dtos/topic/UpdateTopicInputDto.kt
+++ src/main/kotlin/br/com/rodrigo/forum/dtos/topic/UpdateTopicInputDto.kt
@@ -5,13 +5,13 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class UpdateTopicInputDto(
-        @field:NotNull(message = "id não pode ser nulo.")
-        val id: Long,
+    @field:NotNull(message = "id não pode ser nulo.")
+    val id: Long,
 
-        @field:NotEmpty(message = "title não pode ser em branco.")
-        @field:Size(min = 5, max= 100, message = "title deve ter entre 5 e 100 caracteres.")
-        val title: String,
+    @field:NotEmpty(message = "title não pode ser em branco.")
+    @field:Size(min = 5, max = 100, message = "title deve ter entre 5 e 100 caracteres.")
+    val title: String,
 
-        @field:NotEmpty(message = "message não pode ser em branco.")
-        val message: String
+    @field:NotEmpty(message = "message não pode ser em branco.")
+    val message: String
 )
