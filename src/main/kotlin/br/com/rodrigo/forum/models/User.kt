4f4c080cdf614645e0fc8eef68b170a6a5ce9740
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2023-03-03 18:58:29 -0300

    Alter name use entity in database

--- src/main/kotlin/br/com/rodrigo/forum/models/User.kt
+++ src/main/kotlin/br/com/rodrigo/forum/models/User.kt
@@ -2,14 +2,14 @@ package br.com.rodrigo.forum.models
 
 import jakarta.persistence.*
 
-@Entity
+@Entity(name = "users")
 data class User(
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
 
-        @Column(name = "name", nullable = false)
-        val name: String? = null,
+    @Column(name = "name", nullable = false)
+    val name: String? = null,
 
-        @Column(name = "email", nullable = false)
-        val email: String? = null
+    @Column(name = "email", nullable = false)
+    val email: String? = null
 )
